b73576f1fa130c5607e6f3a414f69ea20d0c5e12
Etienne Pelletier <epelletier@maestrodev.com>
2012-12-17 13:05:46 -0600

    Modified for RedHat family since package repo is no longer available.

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,15 +37,57 @@ class nodejs(
           before => Anchor['nodejs::repo'],
         }
       }
+      package { 'nodejs':
+        name    => $nodejs::params::node_pkg,
+        ensure  => present,
+        require => Anchor['nodejs::repo']
+      }
+
+      package { 'npm':
+        name    => $nodejs::params::npm_pkg,
+        ensure  => present,
+        require => Anchor['nodejs::repo']
+      }      
+      
+      if $dev_package and $nodejs::params::dev_pkg {
+        package { 'nodejs-dev':
+          name    => $nodejs::params::dev_pkg,
+          ensure  => present,
+          require => Anchor['nodejs::repo']
+        }
+      }
     }
 
     'Fedora', 'RedHat', 'CentOS', 'Amazon': {
-      package { 'nodejs-stable-release':
-        ensure   => present,
-        source   => $nodejs::params::pkg_src,
-        provider => 'rpm',
-        before   => Anchor['nodejs::repo'],
+      # wget from https://github.com/maestrodev/puppet-wget
+      include wget
+      wget::fetch { "nodejs":
+        source => $pkg_src,
+        destination => $nodejs_tarball,
+        notify => Exec['nodejs_unpack'],
+      } ->
+      exec { 'nodejs_unpack':
+         command => "tar zxf ${nodejs_tarball}",
+         cwd     => '/usr/local',
+         creates => $install_dir,
+      } ->
+      file { '/usr/local/node': 
+        ensure => link,
+        target => $install_dir
+      } ->
+      file { '/usr/bin/node': 
+        ensure => link,
+        target => '/usr/local/node/bin/node',
+      } ->
+      file { '/usr/bin/npm': 
+        ensure => link,
+        target => '/usr/local/node/bin/npm',
+      } ->
+      file { '/usr/bin/node-waf': 
+        ensure => link,
+        target => '/usr/local/node/bin/node-waf',
       }
+      
     }
 
     default: {
@@ -56,18 +98,7 @@ class nodejs(
   # anchor resource provides a consistent dependency for prereq.
   anchor { 'nodejs::repo': }
 
-  package { 'nodejs':
-    name    => $nodejs::params::node_pkg,
-    ensure  => present,
-    require => Anchor['nodejs::repo']
-  }
-
-  package { 'npm':
-    name    => $nodejs::params::npm_pkg,
-    ensure  => present,
-    require => Anchor['nodejs::repo']
-  }
-
+  
   if $proxy {
     exec { 'npm_proxy':
       command => "npm config set proxy ${proxy}",
@@ -76,12 +107,6 @@ class nodejs(
     }
   }
 
-  if $dev_package and $nodejs::params::dev_pkg {
-    package { 'nodejs-dev':
-      name    => $nodejs::params::dev_pkg,
-      ensure  => present,
-      require => Anchor['nodejs::repo']
-    }
-  }
+  
 
 }
