23c6ccff87506cc2f14a9ece95b9b8ce43d3579d
Wassim Salib <wassim87@gmail.com>
2014-09-28 13:50:13 -0600

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,7 @@ class nodejs(
   $dev_package = false,
   $manage_repo = true,
   $proxy       = '',
-  $version     = 'present'
+  $version     = '0.10.32'
 ) inherits nodejs::params {
   #input validation
   validate_bool($dev_package)
