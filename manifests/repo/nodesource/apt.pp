d0460fd39ac947d722490c81a148faa334ea77ed
alejandrobednarik-olx <alejandrob@olx.com>
2015-05-01 07:58:02 -0300

    Update apt::source to work with new version of puppetlabs-apt

--- manifests/repo/nodesource/apt.pp
+++ manifests/repo/nodesource/apt.pp
@@ -10,17 +10,20 @@ class nodejs::repo::nodesource::apt {
 
   if ($ensure == 'present') {
     apt::source { 'nodesource':
-      include_src       => $enable_src,
-      key               => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
-      key_source        => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
+      include           => {
+          'src' => $enable_src,
+        },
       location          => "https://deb.nodesource.com/${url_suffix}",
+      key               => {
+        'id'     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
+        'source' => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
+      },
       pin               => $pin,
       release           => $::lsbdistcodename,
       repos             => 'main',
       required_packages => 'apt-transport-https ca-certificates',
     }
   }
-
   else {
     apt::source { 'nodesource':
       ensure => 'absent',
