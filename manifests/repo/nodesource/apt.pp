56f2df09a1bf116730a1e1c3210d5b1e0ef0111c
alejandrobednarik-olx <alejandrob@olx.com>
2015-05-01 13:17:49 -0300

    Updated puppetlabs-apt version in fixtures.yml

--- manifests/repo/nodesource/apt.pp
+++ manifests/repo/nodesource/apt.pp
@@ -10,13 +10,13 @@ class nodejs::repo::nodesource::apt {
 
   if ($ensure == 'present') {
     apt::source { 'nodesource':
-      include           => {
-          'src' => $enable_src,
-        },
       location          => "https://deb.nodesource.com/${url_suffix}",
       key               => {
         'id'     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
         'source' => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
+      },
+      include           => {
+        'src' => $enable_src,
       },
       pin               => $pin,
       release           => $::lsbdistcodename,
