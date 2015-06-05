b81439ddeba0bf59e27a78b1703243df69cee088
Jan Grewe <jan.grewe@magicinternet.de>
2015-06-05 13:14:56 +0200

    fix indentation

--- manifests/repo/nodesource/apt.pp
+++ manifests/repo/nodesource/apt.pp
@@ -10,17 +10,17 @@ class nodejs::repo::nodesource::apt {
 
   if ($ensure == 'present') {
     apt::source { 'nodesource':
-      include           => {
+      include  => {
         'src' => $enable_src,
       },
-      key               => {
-        'id'      => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
-        'source'  => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
+      key      => {
+        'id'     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
+        'source' => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
       },
-      location          => "https://deb.nodesource.com/${url_suffix}",
-      pin               => $pin,
-      release           => $::lsbdistcodename,
-      repos             => 'main',
+      location => "https://deb.nodesource.com/${url_suffix}",
+      pin      => $pin,
+      release  => $::lsbdistcodename,
+      repos    => 'main',
     }
   }
 
