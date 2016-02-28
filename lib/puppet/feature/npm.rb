35867a649781040ec8926326a2f5537d5ea2c107
imriz <imrizvik@gmail.com>
2016-02-28 13:01:05 +0200

    Fixes #202 - Implement a feature check for npm

---
+++ lib/puppet/feature/npm.rb
@@ -0,0 +1,15 @@
+require 'puppet/util/feature'
+
+Puppet.features.add(:npm) do
+  def self.which(cmd)
+    exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+      exts.each { |ext|
+        exe = File.join(path, "#{cmd}#{ext}")
+        return exe if File.executable?(exe) && !File.directory?(exe)
+      }
+    end
+    nil
+  end
+  !which('npm').nil? && !which('npm').empty?
+end
