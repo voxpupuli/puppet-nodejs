9a8c3f2203b4a237279b50c935d7bb74a402c74b
Matthias Pigulla <mp@webfactory.de>
2016-11-16 14:50:53 +0100

    Use http instead of https (sic) for apt source
    This is a workaround for nodesource/distributions#388. `deb.nodesource.com` moved to CloudFront and some Linux distros (like Ubuntu 14.04) are unable to fetch packages from there because SSL with SNI is required.
    
    A work-around is to fall back to HTTP for the time being. Note that this supposed not to impair package integrity checking, although I cannot tell whether this makes sense if the key is retrieved over HTTP as well.

--- manifests/repo/nodesource/apt.pp
+++ manifests/repo/nodesource/apt.pp
@@ -17,9 +17,9 @@ class nodejs::repo::nodesource::apt {
       },
       key      => {
         'id'     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
-        'source' => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
+        'source' => 'http://deb.nodesource.com/gpgkey/nodesource.gpg.key',
       },
-      location => "https://deb.nodesource.com/node_${url_suffix}",
+      location => "http://deb.nodesource.com/node_${url_suffix}",
       pin      => $pin,
       release  => $::lsbdistcodename,
       repos    => 'main',
