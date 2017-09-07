4ed75d0551154ec36738e9332834a16edd2f8046
William Yardley <wby@axs.com>
2017-09-06 20:22:35 -0700

    Update acceptance tests and add EPEL test case for RedHat acceptance tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,21 +1,7 @@
 require 'beaker-rspec'
+require 'beaker/puppet_install_helper'
 
-# Install Puppet
-unless ENV['RS_PROVISION'] == 'no'
-  # This will install the latest available package on el and deb based
-  # systems fail on windows and osx, and install via gem on other *nixes
-  foss_opts = { default_action: 'gem_install' }
-
-  if default.is_pe?
-    install_pe
-  else
-    install_puppet(foss_opts)
-  end
-
-  hosts.each do |host|
-    on host, "mkdir -p #{host['distmoduledir']}"
-  end
-end
+run_puppet_install_helper
 
 UNSUPPORTED_PLATFORMS = %w[AIX Solaris].freeze
 
@@ -31,7 +17,6 @@ RSpec.configure do |c|
     # Install module and dependencies
     hosts.each do |host|
       copy_module_to(host, source: proj_root, module_name: 'nodejs')
-      shell("/bin/touch #{default['puppetpath']}/hiera.yaml")
       on host, puppet('module install puppetlabs-apt --version 2.0.1'), acceptable_exit_codes: [0, 1]
       on host, puppet('module install gentoo-portage --version 2.0.1'), acceptable_exit_codes: [0, 1]
       on host, puppet('module install chocolatey-chocolatey --version 0.5.2'), acceptable_exit_codes: [0, 1]
