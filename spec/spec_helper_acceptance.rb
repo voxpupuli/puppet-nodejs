7612020f0b0ef850147a00c7236d34f7ab80e16a
William Yardley <wby@axs.com>
2017-09-09 13:35:38 -0700

    Switch acceptance tests to use beaker-module_install_helper, and drop treydock-gpg_key module from acceptance tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,27 +1,20 @@
 require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
 
 run_puppet_install_helper
+install_module_on(hosts)
+install_module_dependencies_on(hosts)
+
+# Additional modules for soft deps
+install_module_from_forge('puppetlabs-apt', '>= 4.1.0 < 5.0.0')
+install_module_from_forge('stahnma-epel', '>= 1.2.0 < 2.0.0')
+install_module_from_forge('chocolatey-chocolatey', '>= 1.2.6 < 2.0.0')
+install_module_from_forge('gentoo-portage', '>= 2.0.1 < 3.0.0')
 
 UNSUPPORTED_PLATFORMS = %w[AIX Solaris].freeze
 
 RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
   # Readable test descriptions
   c.formatter = :documentation
-
-  # Configure all nodes in nodeset
-  c.before :suite do
-    # Install module and dependencies
-    hosts.each do |host|
-      copy_module_to(host, source: proj_root, module_name: 'nodejs')
-      on host, puppet('module install puppetlabs-apt --version 2.0.1'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module install gentoo-portage --version 2.0.1'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module install chocolatey-chocolatey --version 0.5.2'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module install stahnma-epel --version 1.0.0'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module install treydock-gpg_key --version 0.0.3'), acceptable_exit_codes: [0, 1]
-    end
-  end
 end
