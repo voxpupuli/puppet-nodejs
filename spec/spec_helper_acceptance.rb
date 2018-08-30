53de8acae4ea8c7adce45cee13ce0e9ab2ec7006
juniorsysadmin <juniorsysadmin@users.noreply.github.com>
2018-08-30 22:09:09 +1000

    - Add Beaker-related fixes from modulesync

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,10 +1,12 @@
 require 'beaker-rspec'
+require 'beaker-puppet'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
-run_puppet_install_helper
-install_module
-install_module_dependencies
+run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
+install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
+install_module_on(hosts)
+install_module_dependencies_on(hosts)
 
 # Additional modules for soft deps
 install_module_from_forge('puppetlabs-apt', '>= 4.4.0 < 7.0.0')
