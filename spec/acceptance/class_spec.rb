047aebf689f2781d85d5fffa87cfa06b25700901
Arnout Boks <arnout.boks@moxio.com>
2019-03-01 11:11:07 +0100

    Fix idempotence of unquoted secrets
    The fix in https://github.com/voxpupuli/puppet-nodejs/pull/386 fixes
    idempotence for secrets that contain characters (like '=') that cause
    NPM to quote them in npmrc. Secrets that end up unquoted in npmrc are
    not handled correctly however. This fix makes the quotes optional when
    checking if the config value is already set correctly.

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -56,5 +56,24 @@ describe 'nodejs class:', unless: UNSUPPORTED_PLATFORMS.include?(fact('os.family
         end
       end
     end
+
+    context 'set global_config_entry secret unquoted', if: fact('os.family') == 'RedHat' do
+      let :pp do
+        "class { 'nodejs': }; nodejs::npm::global_config_entry { '//path.to.registry/:_secret': ensure => present, value => 'cGFzc3dvcmQ', require => Package[nodejs],}"
+      end
+
+      it_behaves_like 'an idempotent resource'
+
+      describe package('nodejs') do
+        it { is_expected.to be_installed }
+      end
+
+      describe 'npm config' do
+        it 'contains the global_config_entry secret' do
+          npm_output = shell('cat $(/usr/bin/npm config get globalconfig)')
+          expect(npm_output.stdout).to match '//path.to.registry/:_secret=cGFzc3dvcmQ'
+        end
+      end
+    end
   end
 end
