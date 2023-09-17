683e4ec79dd9b8b1e694e24a685d1d363643b4b5
Kenyon Ralph <kenyon@kenyonralph.com>
2023-09-16 17:03:36 -0700

    spec/setup_acceptance_node.pp: remove
    Duplicate of spec/setup_acceptance_helper.pp.

--- spec/setup_acceptance_node.pp
+++
@@ -1,6 +0,0 @@
-# Facter < 4 needs lsb-release for os.distro.codename
-if versioncmp($facts['facterversion'], '4.0.0') < 0 and $facts['os']['family'] == 'Debian' {
-  package { 'lsb-release':
-    ensure => 'installed',
-  }
-}
