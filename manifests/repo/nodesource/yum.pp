7649b6b8afd64875e1b41066fd2dfbf8c637cc40
Evgeni Golov <evgeni@golov.de>
2023-10-28 18:04:27 +0200

    switch to using the nodistro nodesource repos
    the other ones do not contain the latest releases
    (e.g. 20.x contains 20.5 vs 20.9)

--- manifests/repo/nodesource/yum.pp
+++ manifests/repo/nodesource/yum.pp
@@ -1,20 +1,11 @@
 # PRIVATE CLASS: Do not use directly.
 class nodejs::repo::nodesource::yum {
-  $baseurl        = $nodejs::repo::nodesource::baseurl
-  $descr          = $nodejs::repo::nodesource::descr
-  $enable_src     = $nodejs::repo::nodesource::enable_src
   $ensure         = $nodejs::repo::nodesource::ensure
   $priority       = $nodejs::repo::nodesource::priority
   $proxy          = $nodejs::repo::nodesource::proxy
   $proxy_password = $nodejs::repo::nodesource::proxy_password
   $proxy_username = $nodejs::repo::nodesource::proxy_username
-  $source_baseurl = $nodejs::repo::nodesource::source_baseurl
-  $source_descr   = $nodejs::repo::nodesource::source_descr
-
-  $yum_source_enabled = $enable_src ? {
-    true    => '1',
-    default => '0',
-  }
+  $url_suffix     = $nodejs::repo::nodesource::url_suffix
 
   $yum_failovermethod = (versioncmp($facts['os']['release']['major'], '8') >= 0 and $priority == 'absent') ? {
     true    => 'absent',
@@ -38,24 +29,9 @@ class nodejs::repo::nodesource::yum {
     }
 
     yumrepo { 'nodesource':
-      descr           => $descr,
-      baseurl         => $baseurl,
+      descr           => 'Node.js Packages - $basearch',
+      baseurl         => "https://rpm.nodesource.com/pub_${url_suffix}/nodistro/nodejs/\$basearch",
       enabled         => '1',
-      failovermethod  => $yum_failovermethod,
-      gpgkey          => 'file:///etc/pki/rpm-gpg/NODESOURCE-GPG-SIGNING-KEY-EL',
-      gpgcheck        => '1',
-      module_hotfixes => $module_hotfixes,
-      priority        => $priority,
-      proxy           => $proxy,
-      proxy_password  => $proxy_password,
-      proxy_username  => $proxy_username,
-      require         => File['/etc/pki/rpm-gpg/NODESOURCE-GPG-SIGNING-KEY-EL'],
-    }
-
-    yumrepo { 'nodesource-source':
-      descr           => $source_descr,
-      baseurl         => $source_baseurl,
-      enabled         => $yum_source_enabled,
       failovermethod  => $yum_failovermethod,
       gpgkey          => 'file:///etc/pki/rpm-gpg/NODESOURCE-GPG-SIGNING-KEY-EL',
       gpgcheck        => '1',
@@ -78,10 +54,6 @@ class nodejs::repo::nodesource::yum {
 
   else {
     yumrepo { 'nodesource':
-      ensure => 'absent',
-    }
-
-    yumrepo { 'nodesource-source':
       ensure => 'absent',
     }
 
