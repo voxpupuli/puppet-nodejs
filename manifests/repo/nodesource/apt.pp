fb0a41ca38263f6a9ec1c668f72de691f68c4794
Arnaud Launay <alaunay@cusae.com>
2026-01-15 21:43:23 +0100

    update apt source to deb822 format

--- manifests/repo/nodesource/apt.pp
+++ manifests/repo/nodesource/apt.pp
@@ -7,15 +7,26 @@ class nodejs::repo::nodesource::apt {
   include apt
 
   if ($ensure != 'absent') {
+
+    apt::keyring { 'nodesource':
+        source   => 'https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key',
+        dir      => '/usr/share/keyrings',
+        filename => 'nodesource-repo.gpg.key.asc',
+    }
+
     apt::source { 'nodesource':
-      key      => {
-        'name'   => 'nodesource-repo.gpg.key.asc',
-        'source' => 'https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key',
-      },
-      location => "https://deb.nodesource.com/node_${url_suffix}",
-      pin      => $pin,
-      release  => 'nodistro',
-      repos    => 'main',
+      source_format => 'sources',
+      location      => [ "https://deb.nodesource.com/node_${url_suffix}", ],
+      keyring       => '/usr/share/keyrings/nodesource-repo.gpg.key.asc',
+      release       => [ 'nodistro', ],
+      repos         => [ 'main', ],
+      types         => [ 'deb', ],
+      require       => Apt::Keyring['nodesource'],
+    }
+
+    apt::pin { 'nodesource':
+      origin => 'deb.nodesource.com',
+      pin    => $pin,
     }
 
     Apt::Source['nodesource'] -> Package<| tag == 'nodesource_repo' |>
