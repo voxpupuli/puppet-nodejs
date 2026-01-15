085cab8e5269ebc074db08aaf78e6e31bef18ff7
Arnaud Launay <alaunay@cusae.com>
2026-01-15 22:04:29 +0100

    comply with lint style

--- manifests/repo/nodesource/apt.pp
+++ manifests/repo/nodesource/apt.pp
@@ -9,18 +9,18 @@ class nodejs::repo::nodesource::apt {
   if ($ensure != 'absent') {
 
     apt::keyring { 'nodesource':
-        source   => 'https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key',
-        dir      => '/usr/share/keyrings',
-        filename => 'nodesource-repo.gpg.key.asc',
+      source   => 'https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key',
+      dir      => '/usr/share/keyrings',
+      filename => 'nodesource-repo.gpg.key.asc',
     }
 
     apt::source { 'nodesource':
       source_format => 'sources',
-      location      => [ "https://deb.nodesource.com/node_${url_suffix}", ],
+      location      => ["https://deb.nodesource.com/node_${url_suffix}",],
       keyring       => '/usr/share/keyrings/nodesource-repo.gpg.key.asc',
-      release       => [ 'nodistro', ],
-      repos         => [ 'main', ],
-      types         => [ 'deb', ],
+      release       => ['nodistro',],
+      repos         => ['main',],
+      types         => ['deb',],
       require       => Apt::Keyring['nodesource'],
     }
 
