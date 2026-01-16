a3536b512f5acfe831c2ae0560cac1303871682c
Arnaud Launay <alaunay@cusae.com>
2026-01-16 10:31:26 +0100

    Revert "force nodesource version"
    This reverts commit aea9912a47a939fbb7001d7e50b9f5464b6d2775.

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -40,8 +40,14 @@ describe 'nodejs' do
 
     include_examples 'cleanup'
 
-    # Force nodesource version
-    repo_priority = '1000'
+    # Debian 12 contains NodeJS 18, when we test 16 and 18, we need to force the nodesource version
+    # as Debians versions *can* be newer
+    repo_priority =
+      if %w[16 18].include?(nodejs_version) && fact('os.family') == 'Debian' && %w[12 13].include?(fact('os.release.major'))
+        '1000'
+      else
+        '1'
+      end
 
     it_behaves_like 'an idempotent resource' do
       let(:manifest) do
