aa0104534c886c5799469148fa7ec6fc61c4c02d
Arnaud Launay <alaunay@cusae.com>
2026-01-18 17:03:05 +0100

    use new deb822 format

--- manifests/repo/nodesource/apt.pp
+++ manifests/repo/nodesource/apt.pp
@@ -1,21 +1,31 @@
 # PRIVATE CLASS: Do not use directly.
 class nodejs::repo::nodesource::apt {
   $ensure     = $nodejs::repo::nodesource::ensure
-  $pin        = $nodejs::repo::nodesource::pin
+  $priority   = $nodejs::repo::nodesource::priority
   $url_suffix = $nodejs::repo::nodesource::url_suffix
 
   include apt
 
   if ($ensure != 'absent') {
+    apt::keyring { 'nodesource':
+      source   => 'https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key',
+      dir      => '/usr/share/keyrings',
+      filename => 'nodesource-repo.gpg.key.asc',
+    }
+
     apt::source { 'nodesource':
-      key      => {
-        'name'   => 'nodesource-repo.gpg.key.asc',
-        'source' => 'https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key',
-      },
-      location => "https://deb.nodesource.com/node_${url_suffix}",
-      pin      => $pin,
-      release  => 'nodistro',
-      repos    => 'main',
+      source_format => 'sources',
+      location      => ["https://deb.nodesource.com/node_${url_suffix}",],
+      keyring       => '/usr/share/keyrings/nodesource-repo.gpg.key.asc',
+      release       => ['nodistro',],
+      repos         => ['main',],
+      types         => ['deb',],
+      require       => Apt::Keyring['nodesource'],
+    }
+
+    apt::pin { 'nodesource':
+      origin   => 'deb.nodesource.com',
+      priority => $priority,
     }
 
     Apt::Source['nodesource'] -> Package<| tag == 'nodesource_repo' |>
@@ -25,6 +35,18 @@ class nodejs::repo::nodesource::apt {
   else {
     apt::source { 'nodesource':
       ensure => 'absent',
+    }
+    apt::pin { 'nodesource':
+      ensure => 'absent',
+    }
+    apt::keyring { 'nodesource':
+      ensure   => 'absent',
+      dir      => '/usr/share/keyrings',
+      filename => 'nodesource-repo.gpg.key.asc',
+    }
+    file { '/etc/apt/sources.list.d/nodesource.sources':
+      ensure => 'absent',
+      notify => Class['Apt::Update'],
     }
   }
 }
