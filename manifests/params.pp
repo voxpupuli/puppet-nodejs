55468e51341f7d90a039964c77c4b290b3882cfd
Arnaud Launay <alaunay@cusae.com>
2026-01-18 17:05:50 +0100

    use default repo priority to 990 for debian

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@ class nodejs::params {
   $nodejs_package_ensure       = 'installed'
   $repo_ensure                 = 'present'
   $repo_pin                    = undef
-  $repo_priority               = 'absent'
+  $repo_priority               = fact('os.family') == 'Debian' ? '990' : 'absent'
   $repo_proxy                  = 'absent'
   $repo_proxy_password         = 'absent'
   $repo_proxy_username         = 'absent'
